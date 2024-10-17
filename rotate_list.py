00ce7586a56a937c8a9df2be7145fd50b71cbf3f
Kristina <hngyn.kristina@gmail.com>
2024-10-17 14:19:54 -0700

    solved with Weixi

--- rotate_list.py
+++ rotate_list.py
@@ -1,4 +1,15 @@
 # Add your clarifying questions here
 
 def rotate_list(list, shift_by):
-    pass
+    k = shift_by
+
+    if len(list) < shift_by:
+        raise ValueError(f"Number must be less than the length of the list: {len(list)}")
+
+    return list[-k:] + list[:-k]
+
+
+
+assert rotate_list([1, "c", 5], 4) == [1, "c", 5]
+
+assert rotate_list([1, 2, 3, 4, 5], 3) == [3, 4, 5, 1, 2]
--- skyline.py
+++ skyline.py
@@ -1,4 +1,16 @@
 # Add your clarifying questions here
 
 def skyline(building_list):
-    pass
+    set_list = set(building_list)
+    new_list = []
+    initial_num = 0
+
+    for num in list(set_list):
+        if num > initial_num:
+            new_list.append(num)
+            initial_num = num
+    return new_list
+
+
+assert skyline([-1, 1, 3, 7, 7, 3]) == [1, 3, 7]
+assert skyline([1, 10, 3, 5]) == [1, 10]
